c20ad0e14a0c79ce9d75a1945fa00ab1cbaf22de
cwaa079 <77374136+cwaa079@users.noreply.github.com>
2021-03-11 21:25:49 +0900

    Create 7.cpp

---
+++ Chapter8/7.cpp
@@ -0,0 +1,20 @@
+//바닥공사(2*2, 2*1, 1*2)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+int d[1001];
+
+int main()
+{
+	cin >> n;
+
+	d[1] = 1;
+	d[2] = 3;
+	for (int i = 3; i <= n; i++) {
+		d[i] = (d[i - 1] + 2 * d[i - 2]) % 796796;
+	}
+	cout << d[n] << '\n';
+	return 0;
+}
