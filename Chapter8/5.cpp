fe995c83997caac70700dd0c3f223724a9a48a9d
cwaa079 <77374136+cwaa079@users.noreply.github.com>
2021-03-11 17:48:43 +0900

    Create 5.cpp
    d[ ]는 횟수

---
+++ Chapter8/5.cpp
@@ -0,0 +1,24 @@
+//1로 만들기
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+int d[30001];
+
+int main()
+{
+    cin>>n;
+    
+    for(int i=2; i<=n; i++){
+        d[i] = d[i-1]+1;
+        
+        if(d[i]%5==0) d[i] = min(d[i],d[i/5]+1);
+        
+        if(d[i]%3==0) d[i] = min(d[i],d[i/3]+1);
+        
+        if(d[i]%2==0) d[i] = min(d[i],d[i/2]+1);
+    }
+    cout<<d[n];
+    return 0;
+}
